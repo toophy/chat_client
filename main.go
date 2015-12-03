85a1323826b6e102ba39aa464ebbc8b1c2d55690
toophy <toophy@vip.qq.com>
2015-12-03 15:04:48 +0800

    更紧凑的消息处理

--- main.go
+++ main.go
@@ -28,18 +28,6 @@ func (this *MasterThread) On_run() {
 func (this *MasterThread) On_end() {
 }
 
-// 响应网络消息包
-func (this *MasterThread) On_NetPacket(m *toogo.Tmsg_packet) bool {
-	p := new(toogo.PacketReader)
-	p.InitReader(m.Data, uint16(m.Count))
-	msg_len := p.ReadUint16()
-	msg_id := p.ReadUint16()
-	msgLoginRet := new(proto.M2C_login_ret)
-	msgLoginRet.Read(p)
-	fmt.Printf("%d,%d,%-v\n", msg_len, msg_id, msgLoginRet)
-	return true
-}
-
 // 响应网络事件
 func (this *MasterThread) On_NetEvent(m *toogo.Tmsg_net) bool {
 
@@ -107,8 +95,29 @@ func (this *MasterThread) On_NetEvent(m *toogo.Tmsg_net) bool {
 	return true
 }
 
+// 注册消息
+func (this *MasterThread) On_RegistNetMsg() {
+	this.RegistNetMsg(proto.M2C_login_ret_Id, this.on_m2c_login_ret)
+}
+
+func (this *MasterThread) on_m2c_login_ret(pack *toogo.PacketReader, sessionId uint32) bool {
+	msg := new(proto.M2C_login_ret)
+	msg.Read(pack)
+	fmt.Println(msg)
+	return true
+}
+
 func main() {
 	main_thread := new(MasterThread)
-	main_thread.Init_thread(main_thread, toogo.Tid_master, "master", 100, 10000)
+	main_thread.Init_thread(main_thread, toogo.Tid_master, "master", 1000, 100, 10000)
 	toogo.Run(main_thread)
 }
+
+// 消息 read write 增加 defer, 拦截底层的readxxx的panic, 返回读取结果(bool), 根据结果处理这个session
+// 消息注册表移植到threadbase, IThread提供OnRegistNetMsg供继承者使用
+
+// 网络包解包错误及应对, 严重错误达到一定数量, 断开连接
+// Count    错误 (太少,太多), 严重错误1次
+// 长度     错误 (太短,太长), 严重错误1次
+// 消息ID   错误 (不存在的ID,跳到下一个消息), 严重错误1次
+// 消息数据 错误 (太长,跳到下一个消息), 严重错误1次
